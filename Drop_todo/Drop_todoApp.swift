45505564ed3fe7a5f7eb549ff1a9ac54a2ece8c6
liuliuliu0221 <13478454399@163.com>
2026-08-19 12:48:57 +0800

    Initial Commit

---
+++ Drop_todo/Drop_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drop_todoApp.swift
+//  Drop_todo
+//
+//  Created by 羽 on 2026/8/19.
+//
+
+import SwiftUI
+
+@main
+struct Drop_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
